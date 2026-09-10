ecab8d5b998b48e6e31993ee76ce2e83142968e9
tarnongugodwin-stack <tarnongugodwin@gmail.com>
2026-09-10 16:15:57 +0100

    Implement student dashboard login check and logout
    Add student dashboard functionality to check login status and logout.

--- Js/student-dashboard.js
+++ Js/student-dashboard.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const loggedInStudent = JSON.parse(
+        localStorage.getItem("loggedInStudent")
+    );
+
+    if (!loggedInStudent) {
+
+        window.location.href = "student-login.html";
+
+        return;
+    }
+
+    document.getElementById("welcomeStudent").innerHTML =
+        "Welcome, " + loggedInStudent.name;
+
+});
+
+function logoutStudent() {
+
+    localStorage.removeItem("loggedInStudent");
+
+    alert("You have successfully logged out.");
+
+          }
