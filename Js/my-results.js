225a02c0dda15edcd8b72bc3f6b68a1ba926890f
tarnongugodwin-stack <tarnongugodwin@gmail.com>
2026-09-11 09:07:06 +0100

    Display current date on results page
    Add current date display to results page

--- Js/my-results.js
+++ Js/my-results.js
@@ -23,6 +23,8 @@ document.getElementById("printDepartment").textContent =
 
 document.getElementById("printLevel").textContent =
     loggedInStudent.level || "";
+    document.getElementById("printDate").textContent =
+    new Date().toLocaleDateString();
     const results = JSON.parse(
         localStorage.getItem("results")
     ) || [];
