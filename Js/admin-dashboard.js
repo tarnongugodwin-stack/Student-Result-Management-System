75a1abf4d5b6f9d242c6a57dbe9781fcc00b2309
tarnongugodwin-stack <tarnongugodwin@gmail.com>
2026-09-15 17:07:50 +0100

    Add adminLogout function for admin logout feature

--- Js/admin-dashboard.js
+++ Js/admin-dashboard.js
@@ -89,3 +89,10 @@ document.addEventListener("DOMContentLoaded", function () {
         failRate.toFixed(1) + "%";
 
 });
+function adminLogout() {
+
+    localStorage.removeItem("adminLoggedIn");
+
+    window.location.href = "index.html";
+
+        }
